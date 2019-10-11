9853181c2257e8f794587e7feef2f9849757f822
Kevin Tam <kevin@glorat.net>
2019-10-11 15:54:41 +0800

    Add a file upload widget

--- client/src/components/views/MyAside.vue
+++ client/src/components/views/MyAside.vue
@@ -1,23 +1,52 @@
 <template>
-    <aside>
+    <aside class="myaside">
     <ul v-for="menuItems in config.navigation_bar" class="navigation">
         <li v-for="id in menuItems">
             <router-link v-bind:to="'/' + id">{{ config.all_pages[id][0]  }}</router-link>
         </li>
     </ul>
+        <el-upload
+                class=""
+                drag
+                action="/api/postssss/"
+                :before-upload="beforeUpload"
+                >
+            <i class="el-icon-upload"></i>
+            <div class="el-upload__text">Drop file here or<br><em>click to upload</em></div>
+        </el-upload>
     </aside>
 </template>
 
 
-<script>
+<script lang="ts">
+    import axios from 'axios';
+
     export default {
         name: "MyAside",
+        methods: {
+            beforeUpload(file:File) {
+                if (file.name.match(/\.gainstrack$/)) {
+                    console.log(`Trying to upload a ${file.type} of size ${file.size}`);
+                    let reader = new FileReader();
+                    reader.onload = function() {
+                        const text = reader.result;
+                        axios.put('/api/source/', {source : text, filePath: '', entryHash:'', sha256sum:''})
+                            .then(response => console.log('Saved'));
+                    };
+                    reader.onerror = function() {
+                        console.log(reader.result);
+                    };
+                    reader.readAsText(file);
+                }
+                else {
+                    console.log("Can only upload .gainstrack files");
+                }
+                return false;
+            }
+        },
         data: function() {
             return {
                 menuItems: ['foo', 'bar'],
-                url_for: function (x) {
-                    return x;
-                },
                 config: {
                     all_pages: {
                         'balance_sheet': ['Balance Sheet', 'g b'],
@@ -51,6 +80,9 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+    .myaside .el-upload-dragger {
+        width: var(--aside-width);
+        height: auto;
+    }
 </style>
