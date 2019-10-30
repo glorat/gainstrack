ffa9c727e9b106cbba7a4ad8750087900a4dbff8
Kevin Tam <kevin@glorat.net>
2019-10-30 10:25:13 +0800

    fix docker_push

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-export GIT_BRANCH="$(git symbolic-ref HEAD --short 2>/dev/null)"
-if [ "GIT_BRANCH" = "master" ] ; then
+export GIT_BRANCH="$CODEBUILD_WEBHOOK_TRIGGER"
+
+echo "Current git branch is $GIT_BRANCH"
+if [ "$GIT_BRANCH" = "branch/master" ] ; then
   echo Pushing master branch image to docker registry...
   docker tag gainstrack docker.gainstrack.com/gainstrack
   docker push docker.gainstrack.com/gainstrack
