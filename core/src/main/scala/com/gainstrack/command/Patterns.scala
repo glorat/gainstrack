c38544cca50c9c79dc567b6832a499e9db3d0fec
Kevin Tam <kevin@glorat.net>
2019-11-20 23:00:26 +0800

    Fix assetPattern

--- core/src/main/scala/com/gainstrack/command/Patterns.scala
+++ core/src/main/scala/com/gainstrack/command/Patterns.scala
@@ -3,7 +3,7 @@ package com.gainstrack.command
 object Patterns {
   val datePattern = raw"(\d{4}-\d{2}-\d{2})"
   val acctPattern = raw"(\S+)"
-  val assetPattern = raw"([A-Z][A-Z0-9]+)"
+  val assetPattern = raw"([A-Z][A-Z0-9-]+)"
   val balanceMatch = raw"(\S+ \S+)"
 
 }
