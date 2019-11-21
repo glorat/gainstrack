5b9e56d1a0accbd100d91a3faa4d05508724a227
Kevin Tam <kevin@glorat.net>
2019-11-21 22:03:37 +0800

    Clean up logback logging

--- web/src/main/scala/ScalatraBootstrap.scala
+++ web/src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,11 @@
 import org.scalatra._
 import javax.servlet.ServletContext
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext
 
 class ScalatraBootstrap extends LifeCycle {
+  val logger =  LoggerFactory.getLogger(getClass)
 
   override def init(context: ServletContext) {
     implicit val ec :ExecutionContext = ExecutionContext.global
@@ -11,7 +13,7 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new controllers.CommandApiController(), "/api/post/*")
     context.mount(new controllers.ApiController(), "/api/*")
     context.mount(new controllers.ExportController(), "/api/export/*")
-    //context.mount(new controllers.Ledger, "/ledger/*")
+    logger.info("ScalatraBootstrap init complete")
   }
 
 }
