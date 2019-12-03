fc84051a31f1fdfedd3254c864c24dccc8d28e29
Kevin Tam <kevin@glorat.net>
2019-12-03 17:40:40 +0800

    Fix IRR calcs for xccy flows

--- core/src/main/scala/com/gainstrack/report/TimeSeries.scala
+++ core/src/main/scala/com/gainstrack/report/TimeSeries.scala
@@ -1,9 +1,9 @@
 package com.gainstrack.report
 
-case class TimeSeries(name:String, units:Seq[String], dates:Seq[String], values:Seq[String], description: Seq[String])
+case class TimeSeries(name:String, units:Seq[String], dates:Seq[String], values:Seq[String], cvalues:Option[Seq[String]], description: Seq[String])
 
 object TimeSeries {
-  def apply(name:String, units:Seq[String], dates:Seq[String], values:Seq[String]) : TimeSeries = {
-    TimeSeries(name, units, dates, values, dates.map(_ => ""))
+  def apply(name:String, units:Seq[String], dates:Seq[String], values:Seq[String], cvalues:Option[Seq[String]]) : TimeSeries = {
+    TimeSeries(name, units, dates, values, cvalues, dates.map(_ => ""))
   }
 }
