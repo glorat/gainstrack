a4c059d547a06ee52215ef4e57213db91ec132ff
Kevin Tam <kevin@glorat.net>
2019-12-06 09:40:10 +0800

    Refactor and clean Vue bootstrap

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,50 +1,12 @@
 import {Notification} from 'element-ui';
 import store from './store';
+import router from './router';
 import App from './App.vue';
 import Vue from 'vue';
-import VueRouter, { RouteConfig } from 'vue-router';
-import Vuex from 'vuex';
-import CommandSummary from './pages/CommandSummary.vue';
-import Editor from './pages/Editor.vue';
-import IncomeStatement from './pages/IncomeStatement.vue';
-import IrrDetail from './pages/IrrDetail.vue';
-import Journal from './pages/Journal.vue';
-import Prices from './pages/Prices.vue';
 
 Vue.prototype.$notify = Notification;
 
-const routes: RouteConfig[] = [
-  {path: '/balance_sheet', component: () => import(/* webpackChunkName: "BalanceSheet" */ './pages/BalanceSheet.vue'),
-    meta: {title: 'Balance Sheet'}},
-  {path: '/income_statement', component: IncomeStatement, meta: {title: 'Income Statement'}},
-  {path: '/journal', component: Journal, meta: {title: 'Journal'}},
-  {path: '/prices', component: Prices, meta: {title: 'Prices'}},
-  {path: '/editor', component: Editor, meta: {title: 'Editor'}},
-  {path: '/errors', component: () => import('./components/SourceErrors.vue'), meta: {title: 'Editor'}},
-  {path: '/irr', component: () => import('./pages/IrrSummary.vue'), meta: {title: 'IRR'}},
-  {path: '/irr/:accountId', component: IrrDetail, name: 'irr_detail', props: true},
-  {path: '/account/:accountId', component: () => import(/* webpackChunkName: "Account" */ './pages/Account.vue'),
-    name: 'account', props: true
-  },
-  {path: '/command/', component: CommandSummary, meta: {title: 'Commands'}},
-  {path: '/command/:accountId', component: () => import('./pages/Command.vue'), name: 'command', props: true},
-  {path: '/aa', component: () => import('./pages/AssetAllocation.vue'), name: 'aa', meta: {title: 'Asset Allocation'}},
-  {path: '/help', component: () => import('./pages/Markdown.vue'), props: {page: 'help.md'}},
-  {path: '/faq', component: () => import('./pages/Markdown.vue'), props: {page: 'faq.md'}},
-  {path: '/*', component: () => import('./pages/Markdown.vue'), props: {page: 'welcome.md'}},
-];
-
-const router = new VueRouter({
-  routes,
-});
-
-router.afterEach((to, from) => {
-  document.title = (to.meta.title || 'Gainstrack');
-});
-
-Vue.use(VueRouter);
-Vue.use(Vuex);
-
+// Get some state on startup
 store.dispatch('reload');
 
 Vue.config.productionTip = false;
