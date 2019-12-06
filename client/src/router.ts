75be6fd9a44b54c30a26c475133158da514faa68
Kevin Tam <kevin@glorat.net>
2019-12-06 09:40:10 +0800

    Refactor and clean Vue bootstrap

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,23 +1,41 @@
+import CommandSummary from '@/pages/CommandSummary.vue';
+import Editor from '@/pages/Editor.vue';
+import IncomeStatement from '@/pages/IncomeStatement.vue';
+import IrrDetail from '@/pages/IrrDetail.vue';
+import Journal from '@/pages/Journal.vue';
+import Prices from '@/pages/Prices.vue';
 import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './pages/Home.vue';
+import Router, {RouteConfig} from 'vue-router';
 
 Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './pages/About.vue'),
-    },
-  ],
+const routes: RouteConfig[] = [
+  {path: '/balance_sheet', component: () => import(/* webpackChunkName: "BalanceSheet" */ './pages/BalanceSheet.vue'),
+    meta: {title: 'Balance Sheet'}},
+  {path: '/income_statement', component: IncomeStatement, meta: {title: 'Income Statement'}},
+  {path: '/journal', component: Journal, meta: {title: 'Journal'}},
+  {path: '/prices', component: Prices, meta: {title: 'Prices'}},
+  {path: '/editor', component: Editor, meta: {title: 'Editor'}},
+  {path: '/errors', component: () => import('./components/SourceErrors.vue'), meta: {title: 'Editor'}},
+  {path: '/irr', component: () => import('./pages/IrrSummary.vue'), meta: {title: 'IRR'}},
+  {path: '/irr/:accountId', component: IrrDetail, name: 'irr_detail', props: true},
+  {path: '/account/:accountId', component: () => import(/* webpackChunkName: "Account" */ './pages/Account.vue'),
+    name: 'account', props: true
+  },
+  {path: '/command/', component: CommandSummary, meta: {title: 'Commands'}},
+  {path: '/command/:accountId', component: () => import('./pages/Command.vue'), name: 'command', props: true},
+  {path: '/aa', component: () => import('./pages/AssetAllocation.vue'), name: 'aa', meta: {title: 'Asset Allocation'}},
+  {path: '/help', component: () => import('./pages/Markdown.vue'), props: {page: 'help.md'}},
+  {path: '/faq', component: () => import('./pages/Markdown.vue'), props: {page: 'faq.md'}},
+  {path: '/*', component: () => import('./pages/Markdown.vue'), props: {page: 'welcome.md'}},
+];
+
+const router = new Router({
+  routes
 });
+
+router.afterEach((to, from) => {
+  document.title = (to.meta.title || 'Gainstrack');
+});
+
+export default router;
