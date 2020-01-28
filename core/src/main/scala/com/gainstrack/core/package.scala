f834b9243d0c27b06648dc187d7c40183669dde3
Kevin Tam <kevin@glorat.net>
2020-01-28 14:53:35 +0800

    Fix bad graph if no data

--- core/src/main/scala/com/gainstrack/core/package.scala
+++ core/src/main/scala/com/gainstrack/core/package.scala
@@ -1,5 +1,7 @@
 package com.gainstrack
 
+import java.time.LocalDate
+
 package object core {
   import scala.language.implicitConversions
 
@@ -11,6 +13,9 @@ package object core {
   def MinDate : LocalDate = java.time.LocalDate.MIN
   def MaxDate : LocalDate = java.time.LocalDate.MAX
   def now(): ZonedDateTime = java.time.ZonedDateTime.now()
+  // FIXME: Centralising this dangerous function here because there is
+  // clearly a timezone issue
+  def today(): LocalDate = LocalDate.now()
   def parseDate(str:String):LocalDate = java.time.LocalDate.parse(str) // TODO: yyyy-mm-dd
   def parseNumber(str:String): Fraction = spire.math.Rational(BigDecimal(str))
   def zeroFraction:Fraction = spire.math.Rational.zero
