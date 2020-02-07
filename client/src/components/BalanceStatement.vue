ec18a4ba37eed7924f9126656806e4656b8ff430
Kevin Tam <kevin@glorat.net>
2020-02-07 11:37:56 +0800

    bal widget

--- client/src/components/BalanceStatement.vue
+++ client/src/components/BalanceStatement.vue
@@ -1,38 +1,76 @@
 <template>
     <div>
-        <input type="text" name="date" v-model="myCmd.date" v-on:input="inputChanged()">
-        Balance:
-        <input type="text" name="account" v-model="myCmd.accountId" v-on:input="inputChanged()">
-        amount
-        <balance-editor v-model="myCmd.balance"></balance-editor>
+        <div>
+            <el-date-picker
+                    v-model="c.date"
+                    type="date"
+                    value-format="yyyy-MM-dd"
+                    size="mini"
+                    :clearable="false"
+            >
+            </el-date-picker>
+        </div>
+        <div>
+            Account:
+            <account-selector v-model="c.accountId" v-on:input="accountIdChanged"></account-selector>
+        </div>
+        <div>
+            Balance
+            <balance-editor v-model="c.balance"></balance-editor>
+        </div>
     </div>
 </template>
 
 <script>
     import BalanceEditor from './BalanceEditor.vue';
     import {CommandEditorMixin} from '../mixins/CommandEditorMixin';
-    import {cloneDeep} from 'lodash';
+    import AccountSelector from './AccountSelector';
+    import {DatePicker, Input, Option, Select, Switch} from 'element-ui';
 
     export default {
         name: 'BalanceStatement',
         props: {cmd: Object},
         mixins: [CommandEditorMixin],
-        components: {BalanceEditor},
+        components: {
+            BalanceEditor,
+            AccountSelector,
+            'el-date-picker': DatePicker,
+            'el-select': Select,
+            'el-option': Option,
+            'el-switch': Switch,
+            'el-input': Input,
+        },
+        methods: {
+            accountIdChanged() {
+                const all = this.$store.state.summary.accounts;
+                const acct = all.find(x => x.accountId === this.c.accountId);
+                if (acct) {
+                    this.c.balance.ccy = acct.ccy;
+                }
+            },
+        },
         data() {
-            let myVal;
+            let c = {};
             if (this.cmd) {
-                myVal = cloneDeep(this.cmd)
-            } else {
-                myVal = {date: '2000-01-01', accountId: '', balance: {number: 0.0, ccy: 'USD'}};
+                c = {...this.cmd}
             }
-
-            return {
-                myCmd: myVal
-            }
+            c.date = c.date || new Date().toISOString().slice(0, 10);
+            c.balance = c.balance || {number: 0, ccy: ''};
+            c.price = c.price || {number: 0, ccy: ''};
+            c.accountId = c.accountId || '';
+            c.otherAccount = c.otherAccount || '';
+            return {c};
         },
         computed: {
+            isValid() {
+                const c = this.c;
+                return c.accountId && c.date && c.balance.ccy;
+            },
             toGainstrack() {
-                return `${this.myCmd.date} bal ${this.myCmd.accountId} ${this.myCmd.balance.number} ${this.myCmd.balance.ccy}`;
+                if (this.isValid) {
+                    const c = this.c;
+                    return `${c.date} bal ${c.accountId} ${c.balance.number} ${c.balance.ccy}`;
+                }
             }
         }
     }
