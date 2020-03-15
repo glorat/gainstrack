b67efdf1ecab001c2bfee08ed7c4f6ea98b50b76
Kevin Tam <kevin@glorat.net>
2020-03-15 20:20:23 +0800

    Switch to plotly dist to save babel time

--- client/src/components/Plotly.vue
+++ client/src/components/Plotly.vue
@@ -3,11 +3,13 @@
 </template>
 <script>
     // Full bundle
-    import Plotly from 'plotly.js'
+    // import Plotly from 'plotly.js'
     // Partial bundle from full plotly.js
     // import Plotly from 'plotly.js/lib/index-basic'
     // Compiled partial bundle from plotly.js-basic-dist
     // import Plotly from 'plotly.js-basic-dist'
+    // Preprared full dist
+    import Plotly from 'plotly.js-dist'
 
     import debounce from 'lodash/debounce'
     import defaults from 'lodash/defaults'
