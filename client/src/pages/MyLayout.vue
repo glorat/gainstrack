ed607a6e0ab652d4f3d33e607d59fa5f3b793de0
Kevin Tam <kevin@glorat.net>
2020-07-24 17:37:38 +0800

    chore: remove dead layout code

--- client/src/pages/MyLayout.vue
+++ client/src/pages/MyLayout.vue
@@ -39,45 +39,6 @@
     import { Tour } from '../lib/loader'
 
 
-    const menuList = [
-        {
-            icon: 'inbox',
-            label: 'Inbox',
-            separator: true
-        },
-        {
-            icon: 'send',
-            label: 'Outbox',
-            separator: false
-        },
-        {
-            icon: 'delete',
-            label: 'Trash',
-            separator: false
-        },
-        {
-            icon: 'error',
-            label: 'Spam',
-            separator: true
-        },
-        {
-            icon: 'settings',
-            label: 'Settings',
-            separator: false
-        },
-        {
-            icon: 'feedback',
-            label: 'Send Feedback',
-            separator: false
-        },
-        {
-            icon: 'help',
-            iconColor: 'primary',
-            label: 'Help',
-            separator: false
-        }
-    ]
-
     export default {
         name: 'MyQLayout',
         components: { MyAside, Tour, FilterForm },
@@ -93,7 +54,6 @@
         },
         data () {
             return {
-                menuList,
                 left: false,
                 drawer: false,
                 pageTitle: '',
