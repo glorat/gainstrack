89ae4d50ec60a8026d5a3dbd899cefe77b50301b
Kevin Tam <kevin@glorat.net>
2020-08-02 11:19:29 +0800

    chore: add support for firebase auth and related support

--- core/src/main/scala/com/gainstrack/lifecycle/FirebaseFactory.scala
+++ core/src/main/scala/com/gainstrack/lifecycle/FirebaseFactory.scala
@@ -1,11 +1,16 @@
-package com.gainstrack.web
+package com.gainstrack.lifecycle
 
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.{FirebaseApp, FirebaseOptions}
-import net.glorat.ledger.FirestoreLedgerConfig
+import net.glorat.ledger.{FirestoreLedger, FirestoreLedgerConfig}
+import org.json4s.{DefaultFormats, ShortTypeHints}
+
+import scala.concurrent.ExecutionContext
 
 object FirebaseFactory {
   val firestoreConfig = FirestoreLedgerConfig("https://gainstrack-poc.firebaseio.com", "users", "records")
+  implicit val formats = DefaultFormats.withHints(ShortTypeHints(List(classOf[GainstrackEntityDelta]))) + InstantSerializer + UUIDSerializer
+
 
   val options: FirebaseOptions =
     new FirebaseOptions.Builder()
@@ -17,6 +22,10 @@ object FirebaseFactory {
 
   def createCustomToken(uid: String) = {
     import com.google.firebase.auth.FirebaseAuth
-    FirebaseAuth.getInstance.createCustomToken(uid)
+    val auth = FirebaseAuth.getInstance(FirebaseApp.getInstance)
+    auth.createCustomToken(uid)
   }
+
+
+  def createRepo(implicit ec: ExecutionContext) = new FirestoreLedger(firestoreConfig)
 }
