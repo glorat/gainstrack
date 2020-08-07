7959af3c6ab739ea4a93e3f2f752fd8302f24416
Kevin Tam <kevin@glorat.net>
2020-08-07 11:29:10 +0800

    chore: syncOneSymbol should return result

--- quotes/src/main/scala/com/gainstrack/quotes/av/QuoteStore.scala
+++ quotes/src/main/scala/com/gainstrack/quotes/av/QuoteStore.scala
@@ -8,6 +8,7 @@ import scala.concurrent.{ExecutionContext, Future}
 trait QuoteStore {
   def readQuotes(symbol: String)(implicit ec: ExecutionContext): Future[SortedMap[LocalDate, Double]]
 
-  def mergeQuotes(symbol: String, orig: SortedMap[LocalDate, Double], actual: SortedMap[LocalDate, Double])(implicit ec: ExecutionContext): Future[Any]
+  def mergeQuotes(symbol: String, orig: SortedMap[LocalDate, Double], actual: SortedMap[LocalDate, Double])(implicit ec: ExecutionContext): Future[QuotesMergeResult]
 }
 
+case class QuotesMergeResult(inserts: Int, updates: Int, error:Option[String])
