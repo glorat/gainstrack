195e0939c9c90aa1b107e2f7b3d869586ec1e2e8
Kevin Tam <kevin@glorat.net>
2020-08-15 20:08:03 +0800

    chore: quotes mixin gzip support

--- web/src/main/scala/controllers/QuotesController.scala
+++ web/src/main/scala/controllers/QuotesController.scala
@@ -5,13 +5,16 @@ import com.gainstrack.quotes.av.QuoteConfig
 import com.gainstrack.web.{GainstrackJsonSerializers, TimingSupport}
 import org.json4s.Formats
 import org.scalatra.json.JacksonJsonSupport
-import org.scalatra.{NotFound, ScalatraServlet}
+import org.scalatra.{ContentEncodingSupport, NotFound, ScalatraServlet}
 import org.slf4j.LoggerFactory
 
 import scala.concurrent.ExecutionContext
 
 class QuotesController(implicit val ec :ExecutionContext)
-  extends ScalatraServlet with JacksonJsonSupport with TimingSupport {
+  extends ScalatraServlet
+    with JacksonJsonSupport
+    with TimingSupport
+    with ContentEncodingSupport {
   val logger =  LoggerFactory.getLogger(getClass)
 
   protected implicit val jsonFormats: Formats = org.json4s.DefaultFormats ++ GainstrackJsonSerializers.all
