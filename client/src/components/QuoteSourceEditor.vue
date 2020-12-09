c693ffa4d70aaae2c4b676e69a0ccf088622ccdb
Kevin Tam <kevin@glorat.net>
2020-12-09 16:34:42 +0800

    feat: quote source editor can add sources

--- client/src/components/QuoteSourceEditor.vue
+++ client/src/components/QuoteSourceEditor.vue
@@ -1,27 +1,45 @@
 <template>
-  <div>
-    <q-field label="id" stack-label>
-      <template v-slot:control>
-        <div class="self-center full-width no-outline" tabindex="0">{{ qsrc.id }}</div>
-      </template>
-    </q-field>
-    <q-input v-model="qsrc.name" label="Name"></q-input>
-    <q-input v-model="qsrc.ticker" label="Ticker"></q-input>
-    <q-select v-model="qsrc.marketRegion" label="Market Region"
-              :options="marketRegions" emit-value
-    >
-      <template v-slot:option="scope">
-        <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
-          <q-item-section>
-            <q-item-label>{{ scope.opt.value }}</q-item-label>
-            <q-item-label caption>{{ scope.opt.description }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </template>
-    </q-select>
-    <q-input v-model="qsrc.exchange" label="Exchange"></q-input>
-    <q-input v-model="qsrc.ccy" label="Currency"></q-input>
-  </div>
+  <q-card>
+    <q-card-section>
+<!--      <q-field label="id" stack-label>-->
+<!--        <template v-slot:control>-->
+<!--          <div class="self-center full-width no-outline" tabindex="0">{{ qsrc.id }}</div>-->
+<!--        </template>-->
+<!--      </q-field>-->
+      <q-input v-model="qsrc.name" label="Name"></q-input>
+      <q-input v-model="qsrc.ticker" label="Ticker"></q-input>
+      <q-select v-model="qsrc.marketRegion" label="Market Region"
+                :options="marketRegions" emit-value
+      >
+        <template v-slot:option="scope">
+          <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+            <q-item-section>
+              <q-item-label>{{ scope.opt.value }}</q-item-label>
+              <q-item-label caption>{{ scope.opt.description }}</q-item-label>
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-select>
+    </q-card-section>
+    <q-separator></q-separator>
+    <q-card-section title="asdf">
+      <div>External quote sources</div>
+      <div v-for="src in qsrc.sources">
+        <q-input v-model="src.sourceType" label="Source Type"></q-input>
+        <q-input v-model="src.ref" label="Symbol/Ref"></q-input>
+      </div>
+      <q-btn @click="qsrc.sources.push({sourceType:'', ref: ''})" color="primary" label="+"></q-btn>
+    </q-card-section>
+    <q-separator></q-separator>
+    <q-card-section>
+
+      <q-input v-model="qsrc.ccy" label="Currency"></q-input>
+      <q-input v-model="qsrc.exchange" label="Exchange"></q-input>
+
+    </q-card-section>
+
+
+  </q-card>
 </template>
 
 <script lang="ts">
