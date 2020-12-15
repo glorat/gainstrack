aa361ef5583feb2a9c193b5cb75d2427a3b6a611
Kevin Tam <kevin@glorat.net>
2020-12-15 16:10:50 +0800

    chore: allow deletion of quote source exts

--- client/src/components/QuoteSourceEditor.vue
+++ client/src/components/QuoteSourceEditor.vue
@@ -17,8 +17,9 @@
         <enum-select v-model="src.sourceType" label="Source Type" :options="quoteSourceTypes"></enum-select>
         <q-input v-model="src.ref" label="Symbol/Ref"></q-input>
         <q-input v-model="src.meta" label="Additional Params"></q-input>
+        <q-btn label="Delete External Source" color="warning" @click="removeSource(src)"></q-btn>
       </div>
-      <q-btn @click="qsrc.sources.push({sourceType:'', ref: ''})" color="primary" label="+"></q-btn>
+      <q-btn @click="qsrc.sources.push({sourceType:'', ref: ''})" color="primary" label="Add External Source"></q-btn>
     </q-card-section>
     <q-separator></q-separator>
     <q-card-section>
@@ -47,6 +48,12 @@
       return {
         marketRegions,
         quoteSourceTypes
+      }
+    },
+    methods: {
+      removeSource(src: any) {
+        const qsrc: QuoteSource = this.qsrc;
+        qsrc.sources = qsrc.sources.filter(s => s!==src)
       }
     },
     computed: {
