4c506b9c25f00181a13a78443e43bf8e6d587a33
Kevin Tam <kevin@glorat.net>
2020-12-22 08:44:20 +0800

    chore: auth0 to firebase token exchange

--- client/functions/src/index.ts
+++ client/functions/src/index.ts
@@ -15,3 +15,53 @@ export const getAllQuoteSources = functions.region('asia-northeast1').https.onRe
   });
   res.json(ret);
 });
+
+import jwt = require('express-jwt');
+
+import jwks = require('jwks-rsa');
+
+import express = require('express');
+
+
+const config = {
+  AUTH0_DOMAIN: 'gainstrack.auth0.com', // e.g., you.auth0.com
+  AUTH0_API_AUDIENCE: 'https://poc.gainstrack.com', // e.g., http://localhost:1337/
+  // FIREBASE_KEY: './firebase/<Firebase JSON>', // e.g., your-project-firebase-adminsdk-xxxxx-xxxxxxxxxx.json
+  // FIREBASE_DB: '<Firebase Database URL>' // e.g., https://your-project.firebaseio.com
+};
+
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  audience: config.AUTH0_API_AUDIENCE,
+  issuer: `https://${config.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256'],
+});
+
+
+
+const app = express();
+
+app.post('/firebase', jwtCheck, async (req, res) => {
+  // Create UID from authenticated Auth0 user
+  // @ts-ignore
+  const uid = req.user.sub;
+  // Mint token
+  try {
+    const customToken = await admin.auth().createCustomToken(uid);
+    res.json({firebaseToken: customToken})
+  } catch (err) {
+    res.status(500).send({
+      message: 'Something went wrong acquiring a Firebase token.',
+      error: err,
+    })
+  }
+});
+
+
+export const auth = functions.region('asia-northeast1').https.onRequest(app)
