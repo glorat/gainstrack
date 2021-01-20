83b2ff72e705023fa7578d8f5047ad6aa44bad03
Kevin Tam <kevin@glorat.net>
2021-01-20 13:00:08 +0800

    fix: quote source upsert version check

--- client/functions/src/writes.ts
+++ client/functions/src/writes.ts
@@ -8,24 +8,33 @@ export let quoteSourceHistoryCreateHandler = (firestore: FirebaseFirestore.Fires
   const toSave = snapData.payload;
 
   try {
-    // Annotate the projection with history reference
-    toSave.lastUpdate = {
-      timestamp: snap.createTime.toMillis(),
-      uid: snapData.uid,
-      revision: (toSave.lastUpdate?.revision ?? 0) + 1,
-      history: historyId,
-    };
 
     const id = toSave?.id;
     const doc = await firestore.collection('quoteSources').doc(id).get();
     if (doc.exists && doc.data()!.lastUpdate?.revision) {
       if (doc.data()!.lastUpdate?.revision === toSave.lastUpdate?.revision) {
+        // Annotate the projection with history reference
+        toSave.lastUpdate = {
+          timestamp: snap.createTime.toMillis(),
+          uid: snapData.uid,
+          revision: (toSave.lastUpdate?.revision ?? 0) + 1,
+          history: historyId,
+        };
+
         await firestore.collection('quoteSources').doc(id).set(toSave);
       } else {
         // Revision mismatch - dump it
         throw new Error('Revision mismatch');
       }
     } else {
+      // First time saving
+      toSave.lastUpdate = {
+        timestamp: snap.createTime.toMillis(),
+        uid: snapData.uid,
+        revision: 1,
+        history: historyId,
+      };
+
       await firestore.collection('quoteSources').doc(id).set(toSave);
     }
   } catch (e) {
