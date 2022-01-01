4d7c198dacdec841302c8eff8b2c600c17fad0d3
Kevin Tam <kevin@glorat.net>
2022-01-01 23:23:08 +0800

    chore: forecast view using tabs

--- client/src/components/forecast/ForecastView.vue
+++ client/src/components/forecast/ForecastView.vue
@@ -1,34 +1,64 @@
 <template>
   <my-page padding>
-    <div class="row items-start q-pa-md q-gutter-md">
-      <q-card class="">
-        <q-card-section>
-          Enter your annual income and expenses, and the current networth of your saved investment assets
-        </q-card-section>
-        <q-separator inset/>
-        <q-card-section>
-          <q-input label="Income" type="number" v-model.number="input.income"></q-input>
-          <q-input label="Expenses" type="number" v-model.number="input.expenses"></q-input>
-          <q-input label="Savings Rate" type="number" v-on:input="onSavingsRateChange($event)"
-                   :value="100*(input.income-input.expenses) / input.income" suffix="%"></q-input>
-          <q-input label="Networth" type="number" v-model.number="input.networth"></q-input>
-        </q-card-section>
-        <q-card-section>
-          Your retirement objective will be met in year <span class="text-h3">{{ targetYear }}</span>
-        </q-card-section>
-      </q-card>
-      <q-card class="">
-        <q-card-section>The following default assumptions can be adjusted</q-card-section>
-        <q-separator inset/>
-        <q-card-section>
-          <q-input label="Inflation" type="number" v-model.number="strategy.inflation" suffix="%"></q-input>
-          <q-input label="Return on investment" type="number" v-model.number="strategy.roi" suffix="%"></q-input>
-          <q-input label="Retirement target (multiples of annual expenses)" type="number"
-                   v-model.number="strategy.expenseMultiple"></q-input>
-        </q-card-section>
-      </q-card>
+    <q-tabs
+      v-model="tab"
+      dense
+      class="text-grey"
+      active-color="primary"
+      indicator-color="primary"
+      align="justify"
+      narrow-indicator
+    >
+      <q-tab name="basic" label="Basic" />
+      <q-tab name="expenses" label="Expenses" />
+      <q-tab name="income" label="Income" />
+    </q-tabs>
+
+    <q-tab-panels v-model="tab" animated>
+      <q-tab-panel name="basic">
+        <q-card class="">
+          <q-card-section>
+            Enter your annual income and expenses, and the current networth of your saved investment assets
+          </q-card-section>
+          <q-separator inset/>
+          <q-card-section>
+            <q-input label="Income" type="number" v-model.number="input.income"></q-input>
+            <q-input label="Expenses" type="number" v-model.number="input.expenses"></q-input>
+            <q-input label="Savings Rate" type="number" v-on:input="onSavingsRateChange($event)"
+                     :value="100*(input.income-input.expenses) / input.income" suffix="%"></q-input>
+            <q-input label="Networth" type="number" v-model.number="input.networth"></q-input>
+            <q-input label="Retirement target (multiples of annual expenses)" type="number"
+                     v-model.number="strategy.expenseMultiple"></q-input>
+          </q-card-section>
+        </q-card>
+      </q-tab-panel>
+
+      <q-tab-panel name="expenses">
+        <q-card class="">
+          <q-card-section>The following default assumptions can be adjusted</q-card-section>
+          <q-separator inset/>
+          <q-card-section>
+            <q-input label="Inflation" type="number" v-model.number="strategy.inflation" suffix="%"></q-input>
+          </q-card-section>
+        </q-card>
+      </q-tab-panel>
+
+      <q-tab-panel name="income">
+        <q-card class="">
+          <q-card-section>The following default assumptions can be adjusted</q-card-section>
+          <q-separator inset/>
+          <q-card-section>
+            <q-input label="Return on investment" type="number" v-model.number="strategy.roi" suffix="%"></q-input>
+          </q-card-section>
+        </q-card>
+      </q-tab-panel>
+    </q-tab-panels>
+
+    <div v-if="targetYear">
+      Your retirement objective will be met in year <span class="text-h3">{{ targetYear }}</span>
     </div>
-    <q-table class="col-12" :data="forecastEntries" :pagination="pagination"></q-table>
+
+    <q-table :data="forecastEntries" :pagination="pagination" dense></q-table>
   </my-page>
 </template>
 
@@ -45,6 +75,8 @@ export default defineComponent({
     const expenses = 20000;
     const networth = 0;
     const timeunit = LocalDate.now().year();
+    const tab = 'basic';
+
     const input = {
       timeunit,
       income,
@@ -54,7 +86,7 @@ export default defineComponent({
     const strategy = {inflation: 3, roi: 7, expenseMultiple: 25};
 
     const pagination = {rowsPerPage: 30};
-    return {input, pagination, strategy};
+    return {tab, input, pagination, strategy};
 
   },
   computed: {
